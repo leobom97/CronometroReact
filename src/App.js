c3604524f3fa0bec9a557aedd888afbca7daf4bb
Leonardo <leordoria2016@gmail.com>
2021-02-22 22:48:17 -0300

    chronometer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './style.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Importando a imagem do cronometro
+import cronometro from './assets/cronometro.png'; 
+
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      numero: 0,
+      botao: 'iniciar'
+    };
+    this.timer = null;
+    this.iniciar = this.iniciar.bind(this);
+    this.zerar = this.zerar.bind(this);
+  }
+
+  iniciar(){
+    let state = this.state;
+
+    if(this.timer !== null){
+      clearInterval(this.timer);
+      this.timer = null;
+      state.botao = 'iniciar';
+    }else{
+      this.timer = setInterval(()=>{
+        let state = this.state;
+        state.numero += 0.1;
+        this.setState(state);
+      },100);
+      state.botao = 'PAUSAR';
+    }
+
+    this.setState(state);
+  }
+
+  zerar(){
+    if(this.timer !== null){
+      clearInterval(this.timer);
+      this.timer = null;
+    }
+
+    let state = this.state;
+    state.numero = 0;
+    state.botao = 'iniciar';
+    this.setState(state);
+
+  }
+
+  render(){
+    return(
+      <div className="container">
+        <h1>CRONÔMETROJS</h1>
+        <img src={cronometro} className="img" />
+        <a className="timer">{this.state.numero.toFixed(1)}</a>
+        <div className="areaBtn">
+          <a className="botao" onClick={this.iniciar}>{this.state.botao}</a>
+          <a className="botao" onClick={this.zerar}>ZERAR</a>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
